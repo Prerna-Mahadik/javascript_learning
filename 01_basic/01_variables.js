f89d3e4d20b763956aec01dbda499207273482e0
Prerna Mahadik <prernamahadikr@gmail.com>
2023-10-17 15:36:06 +0530

    added basic files

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail = "prerna.google.com"
+var accountPassword = "12345"
+var accountCity = "Jaipur"
+
+// accountID = 2 // Not Allwoed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Banglore"
+
+
+console.log(accountID);
+console.table ([accountID, accountEmail, accountPassword, accountCity])
---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,43 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+// console.log(typeof stringNumber);
+
+// ************************* Operations *********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Prerna"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+console.log(true);
